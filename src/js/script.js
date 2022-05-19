91b53d192c40672c2904e5723b5356151d4bd4c8
systemilc <isaac.systemilc@gmail.com>
2022-05-18 21:45:02 -0300

    Adicionado autopreenchimento campos cadastro

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,56 @@
+$("#btn_frm_newsletter").click(function (event) {
+  event.preventDefault();
+  let emailNewsLetter = $("#inptutEmailNewsLetter").val();
 
+  if (
+    emailNewsLetter.indexOf("@") >= 0 &&
+    emailNewsLetter.indexOf(".") >= 0 &&
+    emailNewsLetter.indexOf(" ") <= 0
+  ) {
+    console.log(emailNewsLetter);
+    $("#modal-mensagem").modal();
+  } else {
+    $("#retornoEmail").html(`E-mail Inválido`);
+  }
+});
 
-$('#btn_frm_newsletter').click(function(event){
-    event.preventDefault();
-    let emailNewsLetter = $('#inptutEmailNewsLetter').val();
+$("#btnBuscaCep").click(function (event) {
+  event.preventDefault();
+  const url = "https://viacep.com.br/ws/";
+  const cep = $("#inputZip").val();
+  const complemento = "/json/";
 
-        if(emailNewsLetter.indexOf('@') >= 0 && emailNewsLetter.indexOf('.') >= 0 && emailNewsLetter.indexOf(' ') <= 0 ){
-            console.log(emailNewsLetter)
-            $("#modal-mensagem").modal();
-        }else{
-                $('#retornoEmail').html(`E-mail Inválido`)
-            }
-        }
+  $.ajax({
+    url: `${url}${cep}${complemento}`,
+    success: function (resultado) {
+      $("#logradouro").val(resultado.logradouro);
+      $("#bairro").val(resultado.bairro);
+      $("#cidade").val(resultado.localidade);
+      $("#uf").val(resultado.uf);
+    },
+  });
+});
 
+$("#btnCadastrar").click(function (event) {
+  event.preventDefault();
 
-   
-        
-    )
+  let email = $("inputEmail").val();
+  let senha = $("inputSenha").val();
+  let cep = $("inputZip").val();
+  let logradouro = $("logradouro").val();
+  let numero = $("numero").val();
+  let bairro = $("bairro").val();
+  let cidade = $("cidade").val();
+  let uf = $("uf").val();
+  let aceite = $("gridCheck").val();
 
-
-    /*    
-     ########  API BUSCA CEP   ########
-
-     https://viacep.com.br/ws/${cep}/json/
-    
-    */
+  console.log(email);
+  console.log(senha);
+  console.log(cep);
+  console.log(logradouro);
+  console.log(numero);
+  console.log(bairro);
+  console.log(cidade);
+  console.log(uf);
+  console.log(aceite);
+});
